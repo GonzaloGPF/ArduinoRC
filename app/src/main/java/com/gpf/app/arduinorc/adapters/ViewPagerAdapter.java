d8154bde8fe450388bf20960984ce920a8a86b66
GonzaloGPF <gonzalopabonflores1@gmail.com>
2015-08-28 17:46:46 +0200

    Minor changes. App working well.

--- app/src/main/java/com/gpf/app/arduinorc/adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/gpf/app/arduinorc/adapters/ViewPagerAdapter.java
@@ -21,7 +21,9 @@ import com.gpf.app.arduinorc.fragments.ReceiverFragment;
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
     CharSequence titles[];
-    int icons[] = {R.drawable.ic_gamepad, R.drawable.ic_receiver, R.drawable.ic_program};
+    int icons[] = {R.drawable.ic_gamepad,
+            R.drawable.ic_receiver,
+            R.drawable.ic_program};
     Context context;
 
     public ViewPagerAdapter(FragmentManager fm, Context context) {
@@ -50,7 +52,7 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     @Override
     public CharSequence getPageTitle(int position) {
         Drawable drawable = ContextCompat.getDrawable(context, icons[position]);
-        drawable.setBounds(0, 0, 72, 72);
+        drawable.setBounds(0, 0, 48, 48);
         ImageSpan imageSpan = new ImageSpan(drawable);
         SpannableString spannableString = new SpannableString(" ");
         spannableString.setSpan(imageSpan, 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
